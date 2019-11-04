168b09a20c7410502534b182cc3e5b0ef1035324
John Evans <jevans97@utk.edu>
2019-11-04 09:36:27 -0500

    Add newly created files from last code gen

---
+++ python-flask/openapi_server/controllers/users_controller.py
@@ -0,0 +1,48 @@
+import connexion
+import six
+
+from openapi_server.models.robots_file import RobotsFile  # noqa: E501
+from openapi_server.models.so_metadata import SOMetadata  # noqa: E501
+from openapi_server.models.sitemap import Sitemap  # noqa: E501
+from openapi_server import util
+
+
+def parse_langingpage(url):  # noqa: E501
+    """Extract schema.org metadata
+
+    Parses landing page to extract schema.org metadata  # noqa: E501
+
+    :param url: URL pointing to langing page to be parsed 
+    :type url: str
+
+    :rtype: SOMetadata
+    """
+    return 'do some magic!'
+
+
+def parse_robots(url):  # noqa: E501
+    """Parses robots.txt to find sitemap(s)
+
+    Given a robots.txt file, parse, and retrieve referenced sitemap documents.  # noqa: E501
+
+    :param url: URL pointing to a robots.txt file
+    :type url: str
+
+    :rtype: RobotsFile
+    """
+    return 'do some magic!'
+
+
+def parse_sitemap(url, maxlocs=None):  # noqa: E501
+    """Parses sitemap.xml
+
+    Parses a sitemap to retrieve entries. # noqa: E501
+
+    :param url: URL pointing to a sitemap xml document.
+    :type url: str
+    :param maxlocs: Maximum number of sitemap locations to return (100) 
+    :type maxlocs: int
+
+    :rtype: Sitemap
+    """
+    return 'do some magic!'
---
+++ python-flask/openapi_server/test/test_users_controller.py
@@ -0,0 +1,72 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+import unittest
+
+from flask import json
+from six import BytesIO
+
+from openapi_server.models.robots_file import RobotsFile  # noqa: E501
+from openapi_server.models.so_metadata import SOMetadata  # noqa: E501
+from openapi_server.models.sitemap import Sitemap  # noqa: E501
+from openapi_server.test import BaseTestCase
+
+
+class TestUsersController(BaseTestCase):
+    """UsersController integration test stubs"""
+
+    def test_parse_langingpage(self):
+        """Test case for parse_langingpage
+
+        Extract schema.org metadata
+        """
+        query_string = [('url', 'url_example')]
+        headers = { 
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/jevans97utk/willitsync/1.0.2/so',
+            method='GET',
+            headers=headers,
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_parse_robots(self):
+        """Test case for parse_robots
+
+        Parses robots.txt to find sitemap(s)
+        """
+        query_string = [('url', 'url_example')]
+        headers = { 
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/jevans97utk/willitsync/1.0.2/robots',
+            method='GET',
+            headers=headers,
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_parse_sitemap(self):
+        """Test case for parse_sitemap
+
+        Parses sitemap.xml
+        """
+        query_string = [('url', 'url_example'),
+                        ('maxlocs', 56)]
+        headers = { 
+            'Accept': 'application/json',
+        }
+        response = self.client.open(
+            '/jevans97utk/willitsync/1.0.2/sitemap',
+            method='GET',
+            headers=headers,
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    unittest.main()
