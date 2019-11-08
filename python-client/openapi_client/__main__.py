5cde8861c026faed9c8f79929948d5f44c80e7c8
datadavev <datadavev@users.noreply.github.com>
2019-11-08 16:21:49 -0500

    first pass at robot api call

--- python-client/openapi_client/__main__.py
+++ python-client/openapi_client/__main__.py
@@ -5,30 +5,42 @@ import logging
 import click
 import click_log
 import openapi_client
+from pprint import pprint
 
 logger = logging.getLogger(__name__)
 click_log.basic_config(logger)
 
 @click.group()
+@click.pass_context
 @click_log.simple_verbosity_option(logger)
-def main():
+def main(ctx):
     logger.debug("in main()")
-    pass
+    ctx.ensure_object(dict)
+    configuration = openapi_client.configuration.Configuration()
+    configuration.host = "http://localhost:8080/jevans97utk/willitsync/1.0.2"
+    client = openapi_client.ApiClient(configuration)
+    ctx.obj['client'] = openapi_client.DevelopersApi(client)
 
 
 @main.command()
+@click.pass_context
 @click.option('-u','--url', required=True, help='Url to try')
-def robot(url):
+def robot(ctx, url):
     logger.debug("Calling robot with url = " + str(url))
-    print("Robot, url=" + url)
+    result = ctx.obj['client'].parse_robots(url)
+    pprint(result)
+    
 
 
 @main.command()
+@click.pass_context
 @click.option('-u','--url', required=True, help='Url to try')
 @click.option('-x','--maxentries', default=100, help='Maximum number of entries to show')
-def sitemap(url, maxentries):
+def sitemap(ctx, url, maxentries):
     logger.debug(f'calling sitemap with url={url} and maxentries={maxentries}')
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    res = main(obj={})
+    print(f"RESULT = {res}")
+    sys.exit(res)
